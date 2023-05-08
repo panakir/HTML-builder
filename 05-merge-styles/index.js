fbebafa6f99b5d2e27c39c2e8ad7a63714b03c2b
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-05-08 18:13:02 +0300

    feat: add solution merge-styles task(Mon, May 8, 2023 18:12)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '', (error) => {
+  if (error) console.error(error.message)
+})
+
+fs.readdir(path.join(__dirname, 'styles'), (error, files) => {
+  if (error) console.error(error.message)
+  files.forEach(file => {
+    fs.stat(path.join(__dirname, 'styles', file), (error, stat) => {
+      if (error) console.error(error.message)
+      if (stat.isFile() && path.extname(file) === '.css') {
+        const stylesArray = []
+        const readFile = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
+        readFile.on('data', data => {
+           fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, (error) => {
+            if (error) console.error(error.message)
+           })
+        })
+      }
+    })
+  })
+})
