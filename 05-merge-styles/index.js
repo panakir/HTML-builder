dde7bd5e532a08c33cd905e99e29276c1e42b5fd
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-05-09 00:45:06 +0300

    refactor: delete unusable const (Tue, MAy 9, 2023 0:44AM)

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -11,7 +11,6 @@ fs.readdir(path.join(__dirname, 'styles'), (error, files) => {
     fs.stat(path.join(__dirname, 'styles', file), (error, stat) => {
       if (error) console.error(error.message)
       if (stat.isFile() && path.extname(file) === '.css') {
-        const stylesArray = []
         const readFile = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
         readFile.on('data', data => {
            fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), data, (error) => {
