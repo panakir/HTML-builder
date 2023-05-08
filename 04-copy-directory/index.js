5da05b47addb81f38acc7e969c8bfe0d14d9e0ff
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-05-08 14:14:19 +0300

    feat: add solution copy-directory task(Mon, May 8, 2023 14:14)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,11 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+const path = require('path')
 
+fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+
+fsPromises.readdir(path.join(__dirname, 'files'))
+.then((files) => files.forEach(file => {
+    fsPromises.copyFile(path.join(__dirname, `files/${file}`), path.join(__dirname, `files-copy/${file}`))
+  })
+)
