0cdfc61e518225abb780fa9b6bdaa50c36c1f9ef
Kiryl Panamarou <panakir.webdev@gmail.com>
2023-05-11 19:33:40 +0300

    fix: fix rewritable directory after deleting files(Tue, May 11, 2023 19:33)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,18 @@
+const { error } = require('console')
 const fs = require('fs')
 const fsPromises = fs.promises
 const path = require('path')
 
 fsPromises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true})
+
+fs.readdir(path.join(__dirname, 'files-copy'), (error, files) => {
+  if(error) console.error(error.message)
+  files.forEach(file => {
+    fs.unlink(path.join(__dirname, 'files-copy', file), (error,) => {
+      if (error) console.error(error.message)
+    })
+  })
+})
 
 fsPromises.readdir(path.join(__dirname, 'files'))
 .then((files) => files.forEach(file => {
